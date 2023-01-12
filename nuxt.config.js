3f37406f3910667141a04d36a7e8d2b84fcd1058
DengSihan <root@dsh.email>
2023-01-12 15:48:46 +0000

    refactor: default social provider

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     runtimeConfig: {
         public: {
             apiBaseURL: process.env.API_BASE_URL || 'http://laravel-nuxt3.test/api',
-            availableSocialProviders: (process.env.AVAILABLE_SOCIAL_PROVIDERS || 'google,facebook').split(','),
+            availableSocialProviders: (process.env.AVAILABLE_SOCIAL_PROVIDERS || 'github').split(','),
         },
     },
 
