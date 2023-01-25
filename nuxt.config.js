43b2bf982f7ebb56f45be7a05054a137b405fc36
DengSihan <root@dsh.email>
2023-01-25 06:07:59 +0000

    chore: port config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
+    server: {
+        port: 3000,
+    },
+
     runtimeConfig: {
         public: {
             apiBaseURL: process.env.API_BASE_URL || 'http://laravel-nuxt3.test/api',
