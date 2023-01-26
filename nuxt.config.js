2eb62cc57897bb29e16e465bd8f697643a49fbff
DengSihan <root@dsh.email>
2023-01-26 13:07:32 +0000

    refactor: nuxt preview port config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-    server: {
-        port: 3000,
-    },
-
     runtimeConfig: {
         public: {
             apiBaseURL: process.env.API_BASE_URL || 'http://laravel-nuxt3.test/api',
