d60487831db0058dc063e772251c5a59c2879b73
DengSihan <root@dsh.email>
2023-01-28 06:52:28 +0000

    docs: add README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Nuxt3 SSR Example with Authorization
+
+Server Side Rendering Nuxt3 with Authorization: [Online Demo](https://laravel-nuxt3.ruti.page)
+
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,20 @@
             class="text-xl font-bold my-8">
             Nuxt3 + Laravel Basic SSR Example with Authorization
         </h1>
+        <p
+            class="my-4">
+            <i
+                class="mdi mdi-github"></i>
+            source code: 
+            <a
+                class="mx-2"
+                target="_blank"
+                href="https://github.com/DengSihan/laravel-nuxt3-web">web(Nuxt3)</a>
+            <a
+                class="mx-2"
+                target="_blank"
+                href="https://github.com/DengSihan/laravel-nuxt3-api">api(Laravel)</a>
+        </p>
         <hr>
         <slot/>
     </main>
